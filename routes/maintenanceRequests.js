746e7df9c9453e6a3d004afb4ec6fbf78a4ae598
ssskram <paul.marks@pittsburghpa.gov>
2018-11-15 16:02:26 -0500

    post request data working, push to production

--- routes/maintenanceRequests.js
+++ routes/maintenanceRequests.js
@@ -2,8 +2,8 @@
 
 const express = require('express')
 const router = express.Router()
-const fetch = require("node-fetch")
-const dt = require("node-json-transform").DataTransform
+const fetch = require('node-fetch')
+const dt = require('node-json-transform').DataTransform
 const models = require('./models/maintenanceRequests')
 const checkToken = require('./../token')
 
@@ -14,12 +14,12 @@ router.get('/allRequests',
   function (req, res) {
     const valid = (checkToken(req.token))
     if (valid == true) {
-      fetch("https://cgweb06.cartegraphoms.com/PittsburghPA/api/v1/classes/cgTasksClass?filter=(([RequestIssue] is not equal to \"\"))&limit=10000&offset=0", {
-          method: 'get',
-          headers: new Headers({
-            'Authorization': 'Basic ' + process.env.CART
-          })
+      fetch('https://cgweb06.cartegraphoms.com/PittsburghPA/api/v1/classes/cgTasksClass?filter=(([RequestIssue] is not equal to ""))&limit=10000&offset=0', {
+        method: 'get',
+        headers: new Headers({
+          'Authorization': 'Basic ' + process.env.CART
         })
+      })
         .then(res => res.json())
         .then(data => {
           res.status(200).send(dt(data, models.allRequests).transform())
@@ -33,12 +33,12 @@ router.get('/allIssues',
   function (req, res) {
     const valid = (checkToken(req.token))
     if (valid == true) {
-      fetch("https://cgweb06.cartegraphoms.com/PittsburghPA/api/v1/Classes/cgRequestIssuesClass?fields=Oid,AppliesTocgFacilitiesField,IssueField,InternalRequestCategoryField,InactiveField&filter=(([AppliesTocgFacilities] is equal to true AND [Inactive] is equal to false))", {
-          method: 'get',
-          headers: new Headers({
-            'Authorization': 'Basic ' + process.env.CART
-          })
+      fetch('https://cgweb06.cartegraphoms.com/PittsburghPA/api/v1/Classes/cgRequestIssuesClass?fields=Oid,AppliesTocgFacilitiesField,IssueField,InternalRequestCategoryField,InactiveField&filter=(([AppliesTocgFacilities] is equal to true AND [Inactive] is equal to false))', {
+        method: 'get',
+        headers: new Headers({
+          'Authorization': 'Basic ' + process.env.CART
         })
+      })
         .then(res => res.json())
         .then(data => {
           res.status(200).send(dt(data, models.issues).transform())
@@ -52,27 +52,42 @@ router.post('/newRequest',
   function (req, res) {
     const valid = (checkToken(req.token))
     if (valid == true) {
-      fetch("https://cgweb06.cartegraphoms.com/PittsburghPA/api/v1/Classes/cgTasksClass", {
-          method: 'POST',
-          headers: new Headers({
-            'Authorization': 'Basic ' + process.env.CART,
-            'Accept': 'application/json',
-            'Content-Type': 'application/json;odata=verbose'
-          }),
-          body: JSON.stringify(req.body)
-        })
-        .then(res => {
-          if (res.status == 200) {
-            return res.status(201).send({
-              success: 'true'
-            })
-          } else {
-            return res.status(201).send({
-              success: 'false'
-            })
-          }
-        })
-    } else res.status(403).end()
+      fetch('https://cgweb06.cartegraphoms.com/PittsburghPA/api/v1/Classes/cgTasksClass', {
+        method: 'POST',
+        headers: new Headers({
+          'Authorization': 'Basic ' + process.env.CART,
+          'Content-Type': 'application/json;odata=verbose'
+        }),
+        body: JSON.stringify(req.body)
+      })
+        .then(response => response.json())
+        .then(data => data.cgTasksClass[0])
+        .then(newItem => newItem.Oid)
+        .then(Oid => res.status(201).send({
+          Oid: Oid
+        }))
+        .catch(error => res.status(500).send(error))
+    }
+    else res.status(403).end()
+  }
+)
+
+// add an image attachment to maintenance request
+router.post('/addImage',
+  function (req, res) {
+    const valid = (checkToken(req.token))
+    if (valid == true) {
+      fetch('https://cgweb06.cartegraphoms.com/PittsburghPA/api/v1/attachments/cgTasksClass/' + req.query.oid + '/cgTasks_cgAttachmentsClass/?fileName=' + req.query.filename, {
+        method: 'POST',
+        headers: new Headers({
+          'Authorization': 'Basic ' + process.env.CART,
+          'Content-Type': 'application/json;odata=verbose'
+        }),
+        body: JSON.stringify(req.body)
+      })
+        .then(response => response.json())
+    }
+    else res.status(403).end()
   }
 )
 

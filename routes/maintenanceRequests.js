da50025575b857988469c0fdef646f3d9c482b86
ssskram <paul.marks@pittsburghpa.gov>
2018-11-16 12:08:43 -0500

    image upload endpoint working

--- routes/maintenanceRequests.js
+++ routes/maintenanceRequests.js
@@ -6,6 +6,19 @@ const fetch = require('node-fetch')
 const dt = require('node-json-transform').DataTransform
 const models = require('./models/maintenanceRequests')
 const checkToken = require('./../token')
+var multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '../uploads'));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.name);
+  }
+})
+let upload = multer({
+  storage: storage
+})
 
 global.Headers = fetch.Headers
 
@@ -15,11 +28,11 @@ router.get('/allRequests',
     const valid = (checkToken(req.token))
     if (valid == true) {
       fetch('https://cgweb06.cartegraphoms.com/PittsburghPA/api/v1/classes/cgTasksClass?filter=(([RequestIssue] is not equal to ""))&limit=10000&offset=0', {
-        method: 'get',
-        headers: new Headers({
-          'Authorization': 'Basic ' + process.env.CART
+          method: 'get',
+          headers: new Headers({
+            'Authorization': 'Basic ' + process.env.CART
+          })
         })
-      })
         .then(res => res.json())
         .then(data => {
           res.status(200).send(dt(data, models.allRequests).transform())
@@ -34,11 +47,11 @@ router.get('/allIssues',
     const valid = (checkToken(req.token))
     if (valid == true) {
       fetch('https://cgweb06.cartegraphoms.com/PittsburghPA/api/v1/Classes/cgRequestIssuesClass?fields=Oid,AppliesTocgFacilitiesField,IssueField,InternalRequestCategoryField,InactiveField&filter=(([AppliesTocgFacilities] is equal to true AND [Inactive] is equal to false))', {
-        method: 'get',
-        headers: new Headers({
-          'Authorization': 'Basic ' + process.env.CART
+          method: 'get',
+          headers: new Headers({
+            'Authorization': 'Basic ' + process.env.CART
+          })
         })
-      })
         .then(res => res.json())
         .then(data => {
           res.status(200).send(dt(data, models.issues).transform())
@@ -53,13 +66,13 @@ router.post('/newRequest',
     const valid = (checkToken(req.token))
     if (valid == true) {
       fetch('https://cgweb06.cartegraphoms.com/PittsburghPA/api/v1/Classes/cgTasksClass', {
-        method: 'POST',
-        headers: new Headers({
-          'Authorization': 'Basic ' + process.env.CART,
-          'Content-Type': 'application/json;odata=verbose'
-        }),
-        body: JSON.stringify(req.body)
-      })
+          method: 'POST',
+          headers: new Headers({
+            'Authorization': 'Basic ' + process.env.CART,
+            'Content-Type': 'application/json;odata=verbose'
+          }),
+          body: JSON.stringify(req.body)
+        })
         .then(response => response.json())
         .then(data => data.cgTasksClass[0])
         .then(newItem => newItem.Oid)
@@ -67,30 +80,26 @@ router.post('/newRequest',
           Oid: Oid
         }))
         .catch(error => res.status(500).send(error))
-    }
-    else res.status(403).end()
+    } else res.status(403).end()
   }
 )
 
 // add an image attachment to maintenance request
 router.post('/addImage',
-  function (req, res) {
+  upload.single(),
+  async function (req, res) {
     const valid = (checkToken(req.token))
     if (valid == true) {
-      console.log(JSON.stringify(req.body))
-      fetch('https://cgweb06.cartegraphoms.com/PittsburghPA/api/v1/attachments/cgTasksClass/' + req.query.oid + '/cgTasks_cgAttachmentsClass/?fileName=' + req.query.filename, {
+      const response = await fetch('https://cgweb06.cartegraphoms.com/PittsburghPA/api/v1/attachments/cgTasksClass/' + req.query.oid + '/cgTasks_cgAttachmentsClass/?fileName=' + req.query.filename, {
         method: 'POST',
         headers: new Headers({
-          'Authorization': 'Basic ' + process.env.CART,
-          'Content-Type': 'application/json;odata=verbose'
+          'Authorization': 'Basic ' + process.env.CART
         }),
-        body: JSON.stringify(req.body)
+        body: req
       })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .then(res.status(201))
-    }
-    else res.status(403).end()
+      const json = await response.json()
+      res.status(201).send(json)
+    } else res.status(403).end()
   }
 )
 
